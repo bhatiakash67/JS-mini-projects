396c4b7711c99985f0730e6d5eb2e7017cf96ef2
bhatiakash67 <akashbhati67@gmail.com>
2024-01-31 00:06:51 +0530

    css styles and score update

--- 4-RPSgame/rps.js
+++ 4-RPSgame/rps.js
@@ -12,27 +12,44 @@ function gameLogic(userChoice, pcChoice) {
         (userChoice === "paper" && pcChoice === "rock") ||
         (userChoice === "scissors" && pcChoice === "paper")) {
         return "You win!!!"
-
     } else {
         return "You lose!!!"
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    const header = document.getElementById("header")
     const userInput = document.getElementById("user-input")
     const pcinput = document.getElementById("pc-input")
     const result = document.getElementById("result")
-
+    const userScore = document.getElementById("user-score")
+    const pcScore = document.getElementById("pc-score")
     const buttons = document.querySelectorAll(".button-container button")
+
+    header.textContent = "rock paper scissors";
+    header.style.textTransform = "uppercase";
+    let userScoreCount = 0;
+    let pcScoreCount = 0;
+
     buttons.forEach(button => {
         button.addEventListener("click", function () {
             const userChoice = this.id
             const pcChoice = computerInput()
-            const
 
-            userInput.textContent = `You chose: ${userChoice}`
-            pcinput.textContent = `The computer chose: ${pcChoice}`
-            result.textContent = gameLogic(userChoice, pcChoice)
+            userInput.textContent = `You chose: ${userChoice.toUpperCase()}`
+            pcinput.textContent = `The computer chose: ${pcChoice.toUpperCase()}`
+
+            const gameResult = gameLogic(userChoice, pcChoice)
+            result.textContent = gameResult;
+
+            if(gameResult === "You win!!!"){
+                userScoreCount++
+            }
+            else if(gameResult === "You lose!!!"){
+                pcScoreCount++
+            }
+            userScore.textContent = `You: ${userScoreCount}`
+            pcScore.textContent = `PC: ${pcScoreCount}`
         })
     })
 })
